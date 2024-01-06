f1404362ad7a192c786bdc2015ab384b291bac66
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-01-06 12:03:13 +0600

    Added modal

--- credit.js
+++ credit.js
@@ -26,3 +26,32 @@ $(document).ready(function () {
     ],
   });
 });
+
+$(document).ready(function () {
+  $('.screenshoots-slider').slick({
+    slidesToShow: 3.6,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    speed: 300,
+    infinite: false,
+    autoplaySpeed: 5000,
+    autoplay: true,
+    nextArrow: $('.slick-next'),
+    prevArrow: $('.slick-prev'),
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
