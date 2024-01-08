9d5f92e2714e36ffd4c3cef439b4f68efcfed8e3
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-01-08 13:18:51 +0600

    Added vimeo video

--- credit.js
+++ credit.js
@@ -55,3 +55,61 @@ $(document).ready(function () {
     ],
   });
 });
+
+$(document).ready(function () {
+  $('.my-slider2').slick({
+    slidesToShow: 3.6,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    speed: 300,
+    infinite: true,
+    autoplaySpeed: 5000,
+    autoplay: true,
+    nextArrow: $('.slick-next'),
+    prevArrow: $('.slick-prev'),
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $('.screenshots-slider2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 300,
+    infinite: true,
+    autoplaySpeed: 5000,
+    autoplay: true,
+    nextArrow: $('.slick-next2'),
+    prevArrow: $('.slick-prev2'),
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
